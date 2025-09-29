b9d3cf73e5ea224a999a517c4a53cc04862ae0c2
lucsa-a <lucas.andre.monteiro.sousa@ccc.ufcg.edu.br>
2025-09-29 00:57:47 -0300

    fix: instala stopwords caso não haja

--- backend/utils.py
+++ backend/utils.py
@@ -4,6 +4,11 @@ from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 import PyPDF2
 
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
 stop_words = set(stopwords.words('portuguese'))
 stemmer = SnowballStemmer('portuguese')
 
